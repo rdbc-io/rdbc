464757646facdfe4f853bea97bca5486ac1ef1cc
Krzysztof Pado <povder@gmail.com>
2017-04-30 21:05:40 +0200

    Introduce scalastyle rules

--- rdbc-tests/src/main/scala/io/rdbc/test/RowPublisherVerification.scala
+++ rdbc-tests/src/main/scala/io/rdbc/test/RowPublisherVerification.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 Krzysztof Pado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.rdbc.test
 
 import java.util.concurrent.atomic.AtomicInteger
@@ -41,7 +57,7 @@ abstract class RowPublisherVerification(env: TestEnvironment, publisherShutdownT
     val currIdx = idx.incrementAndGet()
     conn.statement(s"create table tbl$currIdx (col $intDataType)").get.noParams.execute().get
     conn.beginTx().get
-    for (i <- 1L to elements) yield {
+    for { i <- 1L to elements } yield {
       conn.statement(s"insert into tbl$currIdx(col) values(:v)").get.bind("v" -> i).execute().get
     }
     conn.commitTx().get
