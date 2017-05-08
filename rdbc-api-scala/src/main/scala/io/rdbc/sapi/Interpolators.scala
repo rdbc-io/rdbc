51b825ca2c3448a67409280ba40594562fee40dd
Krzysztof Pado <povder@gmail.com>
2017-05-08 20:48:52 +0200

    Reorganize interpolators

--- rdbc-api-scala/src/main/scala/io/rdbc/sapi/Interpolators.scala
+++ rdbc-api-scala/src/main/scala/io/rdbc/sapi/Interpolators.scala
@@ -16,58 +16,7 @@
 
 package io.rdbc.sapi
 
-private[sapi] trait InterpolatorsTrait {
-
-  implicit class SqlInterpolator(val sc: StringContext) {
-
-    /** A "sql" string interpolator that provides functionality to
-      * create [[SqlWithParams]] instances like:
-      *
-      * {{{
-      *  val id = 10
-      *  val select = conn.select(sql"select * from test where id = \$id")
-      * }}}
-      *
-      * The code above produces `select * from test where id = :p1` statement
-      * with `p1` parameter bound to value `10`.
-      *
-      * The interpolator supports also literal values that allow to dynamically
-      * construct statements. To use this feature, instead of \$, use #\$,
-      * for example:
-      *
-      * {{{
-      *  val id = 10
-      *  val table = "test"
-      *  val select = conn.select(sql"select * from #\$table where id = \$id")
-      * }}}
-      *
-      * The code above produces `select * from test where id = :p1` statement
-      * with `p1` parameter bound to value `10`.
-      */
-    def sql(args: Any*): SqlWithParams = {
-      sc.checkLengths(args)
-
-      var sqlArgs = Vector.empty[Any]
-      val argsIter = args.iterator
-      var sqlArgIdx = 1
-      val sql = new StringBuilder
-
-      sc.parts.foreach { part =>
-        if (part.endsWith("#") && argsIter.hasNext) {
-          sql.append(part.take(part.length - 1))
-          sql.append(argsIter.next)
-        } else if (argsIter.hasNext) {
-          sql.append(part).append(":p").append(sqlArgIdx)
-          sqlArgIdx += 1
-          sqlArgs = sqlArgs :+ argsIter.next
-        } else {
-          sql.append(part)
-        }
-      }
-      SqlWithParams(sql.toString, sqlArgs)
-    }
-  }
-}
+trait InterpolatorsTrait extends SqlInterpolatorTrait
 
 /** String interpolators */
 object Interpolators extends InterpolatorsTrait
