e08edd2bbeecbbffedb411e263063a2dae84c7cc
Krzysztof Pado <povder@gmail.com>
2017-05-18 21:32:50 +0200

    Reorganize build script & prepare for maven central

--- project/Library.scala
+++ project/Library.scala
@@ -1,15 +1,13 @@
 import sbt._
 
-object Versions {
-  val scalatest = "3.0.2"
-}
-
 object Library {
+  private val scalatestVersion = "3.0.2"
+
   val reactiveStreams = "org.reactivestreams" % "reactive-streams" % "1.0.0"
   val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.1.3"
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
-  val scalactic = "org.scalactic" %% "scalactic" % Versions.scalatest
-  val scalatest = "org.scalatest" %% "scalatest" % Versions.scalatest
+  val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
+  val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
   val reactiveStreamsTck = "org.reactivestreams" % "reactive-streams-tck" % "1.0.0"
   val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.4.18"
 }
