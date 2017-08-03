01a9ba7a3b1f6b19d09665147c393d98d837b8ae
Krzysztof Pado <povder@gmail.com>
2017-08-03 20:38:40 +0200

    Update dependencies

--- project/Library.scala
+++ project/Library.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Library {
-  private val scalatestVersion = "3.0.2"
+  private val scalatestVersion = "3.0.3"
 
   val reactiveStreams = "org.reactivestreams" % "reactive-streams" % "1.0.0"
-  val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.1.3"
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
+  val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.1.4"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
   val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
   val reactiveStreamsTck = "org.reactivestreams" % "reactive-streams-tck" % "1.0.0"
