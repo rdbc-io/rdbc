d7d1402d7951b5443613f39fce6e77cbdb3bef39
Krzysztof Pado <povder@gmail.com>
2017-08-11 21:34:39 +0200

    Fix scaladoc of sql interpolator

--- rdbc-api-scala/src/main/scala/io/rdbc/sapi/SqlInterpolatorTrait.scala
+++ rdbc-api-scala/src/main/scala/io/rdbc/sapi/SqlInterpolatorTrait.scala
@@ -24,7 +24,7 @@ trait SqlInterpolatorTrait {
       *
       * {{{
       *  val id = 10
-      *  val select = conn.select(sql"select * from test where id = \$id")
+      *  val stmt = conn.statement(sql"select * from test where id = \$id")
       * }}}
       *
       * The code above produces `select * from test where id = ?` statement
@@ -37,7 +37,7 @@ trait SqlInterpolatorTrait {
       * {{{
       *  val id = 10
       *  val table = "test"
-      *  val select = conn.select(sql"select * from #\$table where id = \$id")
+      *  val stmt = conn.statement(sql"select * from #\$table where id = \$id")
       * }}}
       *
       * The code above produces `select * from test where id = ?` statement
