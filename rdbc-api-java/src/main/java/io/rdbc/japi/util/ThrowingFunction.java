34b6a243297115cb6d0dc6643a67ae585894115f
Krzysztof Pado <povder@gmail.com>
2017-11-15 20:51:33 +0100

    Implement exception unchecking in java API

--- rdbc-api-java/src/main/java/io/rdbc/japi/util/ThrowingFunction.java
+++ rdbc-api-java/src/main/java/io/rdbc/japi/util/ThrowingFunction.java
@@ -18,5 +18,14 @@ package io.rdbc.japi.util;
 
 @FunctionalInterface
 public interface ThrowingFunction<T, R> {
-    R apply(T t) throws Throwable;
+    R applyThrowing(T t) throws Throwable;
+
+    @SuppressWarnings("unchecked")
+    default <E extends Throwable> R apply(T t) throws E {
+        try {
+            return applyThrowing(t);
+        } catch (Throwable ex) {
+            throw (E) ex;
+        }
+    }
 }
