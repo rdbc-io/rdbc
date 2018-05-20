c4f2945c1ca2d3fb87c32ac5065a0861708b441c
Krzysztof Pado <povder@gmail.com>
2018-05-19 22:07:13 -0700

    Update dependencies

--- project/Library.scala
+++ project/Library.scala
@@ -6,12 +6,12 @@ object Library {
 
   val reactiveStreams = "org.reactivestreams" % "reactive-streams" % reactiveStreamsVersion
   val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.1.4"
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
   val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
   val scalamock = "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0"
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
   val reactiveStreamsTck = "org.reactivestreams" % "reactive-streams-tck" % reactiveStreamsVersion
-  val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.11"
-  val java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0"
+  val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.12"
+  val java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0"
   val immutables = "org.immutables" % "value" % "2.5.5"
 }
